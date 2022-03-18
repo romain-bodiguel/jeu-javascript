c829f98ef0425ff5d39dcd116bb1e7430b716bfe
romain-bodiguel <romain.bodiguel@gmail.com>
2022-03-18 11:20:21 +0100

    grille mise en place

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
 var app = {
   init: function() {
-    console.log('init');
+    // console.log('init');
 
     // TODO
     app.drawBoard();
 
     // Event listeners - TODO
   },
+
+  drawBoard: function() {
+    divElement = document.getElementById('board');
+
+    // création des lignes
+    for (let i = 1; i <= 4; i++) {
+      newRowElement = document.createElement('div');
+      newRowElement.classList.add('cellRow');
+      newRowElement.setAttribute('id', 'row' + i);
+      divElement.append(newRowElement);
+
+      // création des colonnes dans chaque ligne
+      for (let i = 1; i <=6; i++) {
+        newCellElement = document.createElement('div');
+        newCellElement.classList.add('cell');
+        newRowElement.append(newCellElement);
+      };
+    };
+  },
+
   handleLaunchScriptButton: function() {
     // TODO
     
@@ -16,6 +36,7 @@ var app = {
       app.codeLineLoop(codeLines, 0);
     }, 2000);
   },
+
   codeLineLoop: function(codeLines, index) {
     // Getting currentLine
     var currentLine = codeLines[index];
@@ -37,9 +58,10 @@ var app = {
       }, 1000);
     }
   },
+
   checkSuccess: function() {
     // TODO display if the game is won or not
-  }
+  },
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
